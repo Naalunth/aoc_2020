f88e8e294b76b5d32543735acb0a8dd4662f0085
Naalunth <me@naalunth.dev>
2020-12-03 12:04:55 +0100

    day 3 cleaned up a little

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -1,7 +1,8 @@
+use anyhow::Context;
 use aoc_runner_derive::{aoc, aoc_generator};
 use bstr::ByteSlice;
-use ndarray::Array2;
-use std::mem::MaybeUninit;
+use ndarray::{Array, Array2};
+use std::iter::successors;
 
 type GeneratorOutput = Array2<MapElement>;
 type PartInput = GeneratorOutput;
@@ -15,19 +16,18 @@ pub enum MapElement {
 
 #[aoc_generator(day3)]
 pub fn generator(input: &[u8]) -> anyhow::Result<GeneratorOutput> {
-    let row_count = bytecount::count(input, b'\n') + 1;
-    let column_count = input.find_byte(b'\n').unwrap();
-    let mut result = Array2::maybe_uninit((row_count, column_count));
-    result
-        .iter_mut()
-        .zip(input.iter().cloned().filter_map(|c| match c {
+    let height = bytecount::count(input, b'\n') + 1;
+    let width = input.find_byte(b'\n').unwrap();
+    input
+        .iter()
+        .filter_map(|&c| match c {
             b'.' => Some(MapElement::Empty),
             b'#' => Some(MapElement::Tree),
             _ => None,
-        }))
-        .for_each(|(array_spot, elem)| *array_spot = MaybeUninit::new(elem));
-
-    Ok(unsafe { result.assume_init() })
+        })
+        .collect::<Array<_, _>>()
+        .into_shape([height, width])
+        .context("reshape failed")
 }
 
 #[aoc(day3, part1)]
@@ -36,29 +36,21 @@ pub fn part_1(input: &PartInput) -> usize {
 }
 
 fn count_trees(map: &Array2<MapElement>, down: usize, right: usize) -> usize {
-    let (height, width) = if let [h, w] = *map.shape() {
-        (h, w)
-    } else {
-        unreachable!()
-    };
+    let height = map.nrows();
+    let width = map.ncols();
 
-    let mut x = 0usize;
-    let mut y = 0usize;
-    let mut count = 0usize;
-
-    while y < height {
-        count += map[(y, x)] as usize;
-        y += down;
-        x = (x + right) % width;
-    }
-
-    count
+    successors(Some((0, 0)), |&(y, x)| {
+        Some((y + down, (x + right) % width))
+    })
+    .take_while(|&(y, _)| y < height)
+    .map(|c| map[c] as usize)
+    .sum()
 }
 
 #[aoc(day3, part2)]
-pub fn part_2(input: &PartInput) -> usize {
+pub fn part_2(input: &PartInput) -> u128 {
     [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
         .iter()
-        .map(|&(down, right)| count_trees(input, down, right))
-        .product()
+        .map(|&(down, right)| count_trees(input, down, right) as u128)
+        .product::<u128>()
 }
