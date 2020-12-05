76093ff1b62e978f719d857b242c6c7d58b826f1
Naalunth <me@naalunth.dev>
2020-12-05 17:13:35 +0100

    day 5 is now even more unsafe and fast

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -3,54 +3,36 @@ use std::hint::unreachable_unchecked;
 
 #[aoc(day5, part1)]
 pub fn part_1(input: &[u8]) -> u16 {
-    parse_input(input).max().unwrap()
+    parse_input(input)
+        .max()
+        .unwrap_or_else(|| unsafe { unreachable_unchecked() })
 }
 
 fn parse_input(input: &[u8]) -> impl Iterator<Item = u16> + '_ {
-    input.split(|&c| c == b'\n').map(id)
+    input
+        .chunks(11)
+        .map(|chunk| id(unsafe { chunk.get_unchecked(..10) }))
 }
 
 fn id(seat: &[u8]) -> u16 {
-    debug_assert!(seat.len() == 10);
-    if seat.len() != 10 {
-        unsafe { unreachable_unchecked() }
+    unsafe {
+        debug_assert!(seat.len() == 10);
+        if seat.len() != 10 {
+            unreachable_unchecked()
+        }
+        let translate = |idx, letter| ((*seat.get_unchecked(idx) == letter) as u16) << (9 - idx);
+        (0..=6).fold(0u16, |acc, idx| acc | translate(idx, b'B'))
+            | (7..=9).fold(0u16, |acc, idx| acc | translate(idx, b'R'))
     }
-    let mut id = 0u16;
-    for letter in seat.iter().take(7) {
-        unsafe { id = id.unchecked_add(id) }
-        id += match letter {
-            b'F' => 0u16,
-            b'B' => 1,
-            _ => {
-                debug_assert!(false);
-                unsafe { unreachable_unchecked() }
-            }
-        };
-    }
-    for letter in seat.iter().skip(7).take(3) {
-        unsafe { id = id.unchecked_add(id) }
-        id += match letter {
-            b'L' => 0u16,
-            b'R' => 1,
-            _ => {
-                debug_assert!(false);
-                unsafe { unreachable_unchecked() }
-            }
-        };
-    }
-    id
 }
 
 #[aoc(day5, part2)]
-pub fn part_2(input: &[u8]) -> u16 {
-    let mut iter = parse_input(input);
+pub fn part_2(input: &[u8]) -> u32 {
+    let mut min = 1024;
+    let mut max = 0;
+    let mut sum = 0;
 
-    let first = iter.next().unwrap();
-    let mut min = first;
-    let mut max = first;
-    let mut sum = first as u32;
-
-    for id in iter {
+    for id in parse_input(input) {
         min = min.min(id);
         max = max.max(id);
         sum += id as u32;
@@ -60,5 +42,5 @@ pub fn part_2(input: &[u8]) -> u16 {
     let max = max as u32;
 
     let target = max * (max + 1) / 2 - (min - 1) * min / 2;
-    (target - sum) as u16
+    target - sum
 }
